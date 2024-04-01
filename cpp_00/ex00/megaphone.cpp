f96a42cdff57dee6c69669ed91e6810c70c4b340
isromero <ismaromeroortega@gmail.com>
2024-04-01 21:27:31 +0200

    Change .vscode/settings.json for using tabs always

--- cpp_00/ex00/megaphone.cpp
+++ cpp_00/ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: isromero <isromero@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/01 20:36:34 by isromero          #+#    #+#             */
-/*   Updated: 2024/04/01 21:19:37 by isromero         ###   ########.fr       */
+/*   Updated: 2024/04/01 21:27:09 by isromero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,18 @@
 
 int main(int argc, char **argv)
 {
-    if (argc == 1)
-        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
-    else
-    {
-        for (int i = 1; argv[i]; i++)
-        {
-            for (int j = 0; argv[i][j]; j++)
-                std::cout << (char)toupper(argv[i][j]);
-            if (i < argc - 1)
-                std::cout << " ";
-        }
-    }
-    std::cout << std::endl;
-    return (0);
+	if (argc == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	else
+	{
+		for (int i = 1; argv[i]; i++)
+		{
+			for (int j = 0; argv[i][j]; j++)
+				std::cout << (char)toupper(argv[i][j]);
+			if (i < argc - 1)
+				std::cout << " ";
+		}
+	}
+	std::cout << std::endl;
+	return (0);
 }
