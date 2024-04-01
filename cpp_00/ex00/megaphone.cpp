53833b025484f1e2aaacbdb89aafd9fdf3cc3cf4
isromero <ismaromeroortega@gmail.com>
2024-04-01 21:25:16 +0200

    Change .gitignore, add custom Makefile for every file, add .vscode/settings.json for formatting + Finish cpp_00/ex00

---
+++ cpp_00/ex00/megaphone.cpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: isromero <isromero@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/01 20:36:34 by isromero          #+#    #+#             */
+/*   Updated: 2024/04/01 21:19:37 by isromero         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+    if (argc == 1)
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+    else
+    {
+        for (int i = 1; argv[i]; i++)
+        {
+            for (int j = 0; argv[i][j]; j++)
+                std::cout << (char)toupper(argv[i][j]);
+            if (i < argc - 1)
+                std::cout << " ";
+        }
+    }
+    std::cout << std::endl;
+    return (0);
+}
