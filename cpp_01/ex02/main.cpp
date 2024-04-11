ee11c2f16682d97b55ea02a1d5c17751e8aaf522
isromero <ismaromeroortega@gmail.com>
2024-04-11 18:10:39 +0200

    cpp01/ex02 finish

---
+++ cpp_01/ex02/main.cpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: isromero <isromero@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/10 19:54:28 by isromero          #+#    #+#             */
+/*   Updated: 2024/04/11 18:10:14 by isromero         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string>
+#include <iostream>
+
+int main()
+{
+	std::string str = "HI THIS IS BRAIN";
+	// Con el puntero apuntamos a la memoria de str para poder modificar su valor y poder tener flexibilidad para cambiar a dónde apuntamos
+	std::string *stringPTR = &str;
+	// La referencia crea un "alias" con la misma dirección de memoria que al que se referencia, y no tiene su propia dirección de memoria
+	std::string &stringREF = str;
+
+	std::cout << "Memory address of the string variable: " << &str << std::endl;
+	std::cout << "Memory address held by stringPTR: " << stringPTR << std::endl;
+	std::cout << "Memory address held by stringREF: " << &stringREF << std::endl;
+
+	std::cout << "Value of the string variable: " << str << std::endl;
+	std::cout << "Value pointed to by stringPTR: " << *stringPTR << std::endl;
+	std::cout << "Value pointed to by stringREF: " << stringREF << std::endl;
+}
