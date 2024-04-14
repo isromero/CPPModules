ed28e9d61346987a1bdcdcde297309a2ab65b657
isromero <ismaromeroortega@gmail.com>
2024-04-14 20:13:32 +0200

    cpp01/ex05 finish

---
+++ cpp_01/ex05/Harl.cpp
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Harl.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: isromero <isromero@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/13 13:47:33 by isromero          #+#    #+#             */
+/*   Updated: 2024/04/14 20:12:33 by isromero         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Harl.hpp"
+
+Harl::Harl() {}
+
+Harl::~Harl() {}
+
+void Harl::complain(std::string const &level)
+{
+	std::string levels[4] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+	functionPtr f[4] = {&Harl::debug, &Harl::info, &Harl::warning, &Harl::error};
+
+	for (size_t i = 0; i < 4; i++)
+	{
+		if (levels[i] == level)
+		{
+			(this->*(f[i]))();
+			return;
+		}
+	}
+	std::cerr << "Error: Invalid complain level" << std::endl;
+}
+
+void Harl::debug()
+{
+	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!" << std::endl;
+}
+
+void Harl::info()
+{
+	std::cout << "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!" << std::endl;
+}
+
+void Harl::warning()
+{
+	std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming for years whereas you started working here since last month." << std::endl;
+}
+
+void Harl::error()
+{
+	std::cout << "This is unacceptable! I want to speak to the manager now." << std::endl;
+}
--- cpp_01/ex05/Harl.hpp
+++ cpp_01/ex05/Harl.hpp
@@ -1,28 +1,35 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
+/*   Harl.hpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: isromero <isromero@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/04/11 18:34:30 by isromero          #+#    #+#             */
-/*   Updated: 2024/04/14 20:04:27 by isromero         ###   ########.fr       */
+/*   Created: 2024/04/13 13:45:35 by isromero          #+#    #+#             */
+/*   Updated: 2024/04/14 20:12:39 by isromero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "Harl.hpp"
+#ifndef HARL_HPP
+#define HARL_HPP
 
-#include "Harl.hpp"
 #include <iostream>
 
-int main(int argc, char **argv)
+class Harl
 {
-	if (argc != 2)
-	{
-		std::cerr << "Error: Usage: ./harl <DEBUG|INFO|WARNING|ERROR>" << std::endl;
-		return (1);
-	}
-	Harl harl;
-	harl.complain(argv[1]);
-	return (0);
-}
+public:
+	Harl();
+	~Harl();
+	void complain(std::string const &level);
+
+private:
+	void debug();
+	void info();
+	void warning();
+	void error();
+	// Creamos un alias llamado functionPtr que servirá para
+	// apuntar a cualquier función de Harl, y sin argumentos
+	typedef void (Harl::*functionPtr)();
+};
+
+#endif
