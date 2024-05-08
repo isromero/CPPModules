9eb52f329122854fd37f6d5d2aac962762aad4d2
isromero <ismaromeroortega@gmail.com>
2024-05-08 21:18:33 +0200

    cpp_06/ex00 setup

---
+++ cpp_06/ex00/ScalarConverter.cpp
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScalarConverter.cpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: isromero <isromero@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/08 21:01:35 by isromero          #+#    #+#             */
+/*   Updated: 2024/05/08 21:15:54 by isromero         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ScalarConverter.hpp"
+
+ScalarConverter::ScalarConverter()
+{
+}
+
+ScalarConverter::ScalarConverter(const ScalarConverter &other)
+{
+	*this = other;
+}
+
+ScalarConverter &ScalarConverter::operator=(const ScalarConverter &other)
+{
+	(void)other;
+	return (*this);
+}
+
+ScalarConverter::~ScalarConverter()
+{
+}
+
+void ScalarConverter::convert(std::string literal)
+{
+	std::cout << "char: " << std::endl;
+	std::cout << "int: " << std::endl;
+	std::cout << "float: " << std::endl;
+	std::cout << "double: " << std::endl;
+}
---
+++ cpp_06/ex00/ScalarConverter.hpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScalarConverter.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: isromero <isromero@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/08 20:59:59 by isromero          #+#    #+#             */
+/*   Updated: 2024/05/08 21:04:58 by isromero         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SCALARCONVERTER_HPP
+#define SCALARCONVERTER_HPP
+
+#include <iostream>
+#include <string>
+
+class ScalarConverter
+{
+public:
+	ScalarConverter(const ScalarConverter &other);
+	ScalarConverter &operator=(const ScalarConverter &other);
+	~ScalarConverter();
+
+	static void convert(std::string literal);
+
+private:
+	ScalarConverter();
+};
+
+#endif
